3a0dc565b69ea157b4143c910dd049d71fe52852
Patrique Legault <patrique.legault@aftia.com>
2021-09-23 21:41:40 -0400

    Proper folders

--- core/src/main/java/com/aftia/adobe/core/DocAuthentication.java
+++ core/src/main/java/com/aftia/adobe/core/DocAuthentication.java
@@ -3,5 +3,6 @@ package com.aftia.adobe.core;
 import com.adobe.pdfservices.operation.auth.Credentials;
 
 public interface DocAuthentication {
+    
     public Credentials getCredentials() throws Exception;
 }
